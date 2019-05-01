6764abe9b8920bd86dec4b1b28715c25b4f32573
khaleel1981 <47736806+khaleel1981@users.noreply.github.com>
2019-05-01 15:34:48 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("These is Test Application used for Jenkins!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" is not \"These is Test Application used for Jenkins!\"");
         }
     }
 
